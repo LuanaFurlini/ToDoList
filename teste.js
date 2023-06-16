8f7c1d859f43ac1a734ddae09049980ff3b5d48a
LuanaFurlini <124948459+LuanaFurlini@users.noreply.github.com>
2023-06-16 08:12:43 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,55 @@
+var listaToDo = [];
+function openEditModal() {
+    var modal = document.querySelector('#editar');
+    modal === null || modal === void 0 ? void 0 : modal.showModal();
+}
+function closeEditModal() {
+    var modal = document.querySelector('#editar');
+    modal === null || modal === void 0 ? void 0 : modal.close();
+}
+function openDeleteModal() {
+    var modal = document.querySelector('#excluir');
+    modal === null || modal === void 0 ? void 0 : modal.showModal();
+}
+function closeDeleteModal() {
+    var modal = document.querySelector('#excluir');
+    modal === null || modal === void 0 ? void 0 : modal.close();
+}
+function handleToDo() {
+    var input = document.querySelector('#tarefa');
+    if (input.value !== "") {
+        var lista = document.querySelector('ul');
+        var li = document.createElement('li');
+        var div = document.createElement('div');
+        var btn_edit = document.createElement('button');
+        btn_edit.setAttribute('class', 'botoes');
+        var icon_edit = document.createElement('span');
+        icon_edit.setAttribute('class', 'material-symbols-outlined');
+        icon_edit.textContent = 'Edit';
+        icon_edit.addEventListener('click', openEditModal);
+        btn_edit.appendChild(icon_edit);
+        var btn_delete = document.createElement('button');
+        btn_delete.setAttribute('class', 'botoes');
+        var icon_delete = document.createElement('span');
+        icon_delete.setAttribute('class', 'material-symbols-outlined');
+        icon_delete.textContent = 'delete';
+        icon_delete.addEventListener('click', openDeleteModal);
+        btn_delete.appendChild(icon_delete);
+        div.appendChild(btn_edit);
+        div.appendChild(btn_delete);
+        var card = document.createElement('article');
+        var texto = document.createTextNode(input.value);
+        li.appendChild(card);
+        card.appendChild(texto);
+        card.appendChild(div);
+        lista.appendChild(li);
+        listaToDo.push(input.value);
+    }
+    input.value = "";
+    console.log(listaToDo);
+}
+function editToDo() {
+    for (var i = 0; i <= listaToDo.length; i++) {
+        console.log(listaToDo[i]);
+    }
+}
---
+++ teste.ts
@@ -0,0 +1,96 @@
+
+let listaToDo: string[] = [];
+
+function openEditModal(){
+    let modal: HTMLDialogElement | null 
+    = document.querySelector('#editar')!;
+
+    modal?.showModal();
+}
+
+function closeEditModal(){
+    let modal: HTMLDialogElement | null 
+    = document.querySelector('#editar')!;
+
+    modal?.close()
+}
+
+function openDeleteModal(){
+    let modal: HTMLDialogElement | null 
+    = document.querySelector('#excluir')!;
+    
+    modal?.showModal();
+}
+
+function closeDeleteModal(){
+    let modal: HTMLDialogElement | null 
+    = document.querySelector('#excluir')!;
+
+    modal?.close()
+}
+
+function handleToDo(){
+    let input: HTMLInputElement | null = 
+    document.querySelector('#tarefa')!;
+
+    if(input.value !== ""){
+        let lista: HTMLUListElement | null =
+        document.querySelector('ul')!;
+
+        let li: HTMLLIElement = 
+        document.createElement('li');
+
+        let div: HTMLDivElement =
+        document.createElement('div');
+
+        let btn_edit: HTMLButtonElement = 
+        document.createElement('button');
+        btn_edit.setAttribute('class','botoes');
+
+        let icon_edit: HTMLSpanElement =
+        document.createElement('span');
+        icon_edit.setAttribute('class','material-symbols-outlined');
+        icon_edit.textContent = 'Edit';
+        icon_edit.addEventListener('click', openEditModal)
+        
+        btn_edit.appendChild(icon_edit);
+        
+        let btn_delete: HTMLButtonElement = 
+        document.createElement('button');
+        btn_delete.setAttribute('class','botoes');
+
+        let icon_delete: HTMLSpanElement =
+        document.createElement('span');
+        icon_delete.setAttribute('class','material-symbols-outlined');
+        icon_delete.textContent = 'delete';
+        icon_delete.addEventListener('click', openDeleteModal)
+        
+        btn_delete.appendChild(icon_delete);
+
+        div.appendChild(btn_edit);
+        div.appendChild(btn_delete);
+
+        let card: HTMLElement =
+        document.createElement('article');
+
+        let texto = 
+        document.createTextNode(input.value);
+
+        li.appendChild(card);
+        card.appendChild(texto);
+        card.appendChild(div);
+
+        lista.appendChild(li);
+
+        listaToDo.push(input.value);
+    }
+
+    input.value = "";
+    console.log(listaToDo)
+}
+
+function editToDo(){
+    for(let i = 0; i <= listaToDo.length; i++){
+        console.log(listaToDo[i])
+    }
+}
